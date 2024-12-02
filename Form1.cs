e0e38a22e65982441391fe97cbfeb6bfca255e19
IniDani <msaarafli007@gmail.com>
2024-12-02 08:31:38 +0700

    feat: Add NpgSQL connection to PostgreSQL database

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Npgsql;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,19 @@ namespace Responsi_497787
         public Form1()
         {
             InitializeComponent();
+        }
+        private NpgsqlConnection conn;
+        string connstring = String.Format("Server={0};Port={1};" +
+            "User Id={2};Password={3};Database={4};",
+            "localhost", 5432, "postgres", "12345678", "Responsi_497787");
+        public DataTable dt;
+        public static NpgsqlCommand cmd;
+        private string sql = null;
+        private DataGridViewRow r;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            conn = new NpgsqlConnection(connstring);
         }
     }
 }
