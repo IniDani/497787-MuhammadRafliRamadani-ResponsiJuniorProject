84209de64bfd5f9af58175c1cddadc916e229a1e
IniDani <msaarafli007@gmail.com>
2024-12-02 08:27:46 +0700

    feat: Add form UI

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,20 +32,146 @@ namespace Responsi_497787
         /// </summary>
         private void InitializeComponent()
         {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.logotxt = new System.Windows.Forms.TextBox();
+            this.namakaryawantxt = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.NamaKaryawantb = new System.Windows.Forms.TextBox();
+            this.DepKaryawantb = new System.Windows.Forms.TextBox();
+            this.Insertbtn = new System.Windows.Forms.Button();
+            this.Editbtn = new System.Windows.Forms.Button();
+            this.Deletebtn = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(36, 37);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // logotxt
+            // 
+            this.logotxt.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.logotxt.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.logotxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.logotxt.Location = new System.Drawing.Point(54, 24);
+            this.logotxt.Name = "logotxt";
+            this.logotxt.Size = new System.Drawing.Size(100, 16);
+            this.logotxt.TabIndex = 1;
+            this.logotxt.Text = "logo";
+            // 
+            // namakaryawantxt
+            // 
+            this.namakaryawantxt.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.namakaryawantxt.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.namakaryawantxt.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.namakaryawantxt.Location = new System.Drawing.Point(12, 70);
+            this.namakaryawantxt.Name = "namakaryawantxt";
+            this.namakaryawantxt.Size = new System.Drawing.Size(120, 16);
+            this.namakaryawantxt.TabIndex = 2;
+            this.namakaryawantxt.Text = "Nama Karyawan :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.textBox1.Location = new System.Drawing.Point(12, 107);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(120, 16);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "Dep. Karyawan   \r\n:";
+            // 
+            // NamaKaryawantb
+            // 
+            this.NamaKaryawantb.Location = new System.Drawing.Point(122, 69);
+            this.NamaKaryawantb.Name = "NamaKaryawantb";
+            this.NamaKaryawantb.Size = new System.Drawing.Size(162, 20);
+            this.NamaKaryawantb.TabIndex = 4;
+            // 
+            // DepKaryawantb
+            // 
+            this.DepKaryawantb.Location = new System.Drawing.Point(122, 107);
+            this.DepKaryawantb.Name = "DepKaryawantb";
+            this.DepKaryawantb.Size = new System.Drawing.Size(162, 20);
+            this.DepKaryawantb.TabIndex = 5;
+            // 
+            // Insertbtn
+            // 
+            this.Insertbtn.Location = new System.Drawing.Point(12, 154);
+            this.Insertbtn.Name = "Insertbtn";
+            this.Insertbtn.Size = new System.Drawing.Size(99, 23);
+            this.Insertbtn.TabIndex = 6;
+            this.Insertbtn.Text = "Insert";
+            this.Insertbtn.UseVisualStyleBackColor = true;
+            // 
+            // Editbtn
+            // 
+            this.Editbtn.Location = new System.Drawing.Point(185, 154);
+            this.Editbtn.Name = "Editbtn";
+            this.Editbtn.Size = new System.Drawing.Size(99, 23);
+            this.Editbtn.TabIndex = 7;
+            this.Editbtn.Text = "Edit";
+            this.Editbtn.UseVisualStyleBackColor = true;
+            // 
+            // Deletebtn
+            // 
+            this.Deletebtn.Location = new System.Drawing.Point(347, 154);
+            this.Deletebtn.Name = "Deletebtn";
+            this.Deletebtn.Size = new System.Drawing.Size(99, 23);
+            this.Deletebtn.TabIndex = 8;
+            this.Deletebtn.Text = "Delete";
+            this.Deletebtn.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 192);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(434, 234);
+            this.dataGridView1.TabIndex = 9;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(463, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.Deletebtn);
+            this.Controls.Add(this.Editbtn);
+            this.Controls.Add(this.Insertbtn);
+            this.Controls.Add(this.DepKaryawantb);
+            this.Controls.Add(this.NamaKaryawantb);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.namakaryawantxt);
+            this.Controls.Add(this.logotxt);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
+        private TextBox logotxt;
+        private TextBox namakaryawantxt;
+        private TextBox textBox1;
+        private TextBox NamaKaryawantb;
+        private TextBox DepKaryawantb;
+        private Button Insertbtn;
+        private Button Editbtn;
+        private Button Deletebtn;
+        private DataGridView dataGridView1;
     }
 }
 
